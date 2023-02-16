ae5a91b048d3489e2762857426bd13a0d3af7962
raisadiakbar <muhrais791@gmail.com>
2023-02-16 19:11:54 +0900

    ...

--- main.go
+++ main.go
@@ -1,31 +1,31 @@
-package main
+// package main
 
-import (
-	"fmt"
-	"log"
-	"net/http"
-	"time"
-)
+// import (
+// 	"fmt"
+// 	"log"
+// 	"net/http"
+// 	"time"
+// )
 
-func main() {
+// func main() {
 
-	// Start the server
-	fmt.Println("Server is listening on port 8080...")
-	srv := &http.Server{
-		Handler:      r,
-		Addr:         "127.0.0.1:8080",
-		WriteTimeout: 15 * time.Second,
-		ReadTimeout:  15 * time.Second,
-	}
-	log.Fatal(srv.ListenAndServe())
-}
+// 	// Start the server
+// 	fmt.Println("Server is listening on port 8080...")
+// 	srv := &http.Server{
+// 		Handler:      r,
+// 		Addr:         "127.0.0.1:8080",
+// 		WriteTimeout: 15 * time.Second,
+// 		ReadTimeout:  15 * time.Second,
+// 	}
+// 	log.Fatal(srv.ListenAndServe())
+// }
 
-// Register handler
-func registerHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Register")
-}
+// // Register handler
+// func registerHandler(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprintln(w, "Register")
+// }
 
-// Login handler
-func loginHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Login")
-}
+// // Login handler
+// func loginHandler(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprintln(w, "Login")
+// }
