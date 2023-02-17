612a5e87408775c3cc3258ac6eed37defa1f5c28
raisadiakbar <muhrais791@gmail.com>
2023-02-17 18:00:02 +0900

    main.go

--- main.go
+++ main.go
@@ -1,31 +1,12 @@
-// package main
+package main
 
-// import (
-// 	"fmt"
-// 	"log"
-// 	"net/http"
-// 	"time"
-// )
+import "fmt"
 
-// func main() {
+func main() {
+	var name string
 
-// 	// Start the server
-// 	fmt.Println("Server is listening on port 8080...")
-// 	srv := &http.Server{
-// 		Handler:      r,
-// 		Addr:         "127.0.0.1:8080",
-// 		WriteTimeout: 15 * time.Second,
-// 		ReadTimeout:  15 * time.Second,
-// 	}
-// 	log.Fatal(srv.ListenAndServe())
-// }
+	fmt.Print("i am rais")
+	fmt.Scanln(&name)
 
-// // Register handler
-// func registerHandler(w http.ResponseWriter, r *http.Request) {
-// 	fmt.Fprintln(w, "Register")
-// }
-
-// // Login handler
-// func loginHandler(w http.ResponseWriter, r *http.Request) {
-// 	fmt.Fprintln(w, "Login")
-// }
+	fmt.Printf("Halo, %s!\n", name)
+}
