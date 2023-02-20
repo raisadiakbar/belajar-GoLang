b785ac304cce48e8687bbe4e406bb18ee9a03c18
raisadiakbar <muhrais791@gmail.com>
2023-02-20 20:13:34 +0900

    address

--- controllers/address.go
+++ controllers/address.go
@@ -5,82 +5,106 @@ import (
 	"net/http"
 	"strconv"
 
+	"e-GoLang/errors"
+
+	models "e-GoLang/models"
+
 	"github.com/gorilla/mux"
-	"github.com/raisadiakbar/belajar-GoLang/models"
-	"github.com/raisadiakbar/belajar-GoLang/repository"
+
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
-type AddressController struct{}
-
-var addressRepository = repository.AddressRepository{}
-
-// RespondWithError writes an error response to the http.ResponseWriter
-func respondWithError(w http.ResponseWriter, status int, message string) {
-	response := map[string]string{"error": message}
-	responseJSON, _ := json.Marshal(response)
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(status)
-	w.Write(responseJSON)
-}
-
-// RespondWithJSON writes a JSON response to the http.ResponseWriter
-func respondWithJSON(w http.ResponseWriter, status int, payload interface{}) {
-	response, err := json.Marshal(payload)
-	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, err.Error())
-		return
-	}
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(status)
-	w.Write(response)
-}
-
-// Create a new address
-func (ac AddressController) CreateAddress(w http.ResponseWriter, r *http.Request) {
-	address := models.Address{}
+// createAddressHandler is the handler function for the POST /api/addresses route.
+func createAddressHandler(w http.ResponseWriter, r *http.Request) {
+	// Parse request body
+	var address models.Address
 	err := json.NewDecoder(r.Body).Decode(&address)
 	if err != nil {
-		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	if err = address.Validate(); err != nil {
-		respondWithError(w, http.StatusBadRequest, err.Error())
-		return
-	}
-
-	if err = addressRepository.Create(&address); err != nil {
-		respondWithError(w, http.StatusInternalServerError, err.Error())
-		return
-	}
-
-	respondWithJSON(w, http.StatusCreated, address)
-}
-
-// Get an address by ID
-func (ac AddressController) GetAddress(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	id, err := strconv.Atoi(params["id"])
+	// Create address
+	err = models.DB.Create(&address).Error
 	if err != nil {
-		respondWithError(w, http.StatusBadRequest, "Invalid address ID")
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	address, err := addressRepository.FindByID(id)
-	if err != nil {
-		respondWithError(w, http.StatusNotFound, "Address not found")
-		return
-	}
-
-	respondWithJSON(w, http.StatusOK, address)
+	// Return created address
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(address)
 }
 
-func createAddressHandler(w http.ResponseWriter, r *http.Request) {
-	ac := AddressController{}
-	ac.CreateAddress(w, r)
-}
-
+// getAddressHandler is the handler function for the GET /api/addresses/{id} route.
 func getAddressHandler(w http.ResponseWriter, r *http.Request) {
-	ac := AddressController{}
-	ac.GetAddress(w, r)
+	// Get address ID from route parameters
+	vars := mux.Vars(r)
+	addressID, err := strconv.ParseUint(vars["id"], 10, 32)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// Get address by ID
+	var address models.Address
+	err = models.DB.First(&address, addressID).Error
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+
+	// Return address
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(address)
+}
+
+func updateAddressHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	addressID, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		http.Error(w, "Invalid address ID", http.StatusBadRequest)
+		return
+	}
+
+	var address models.Address
+	err = json.NewDecoder(r.Body).Decode(&address)
+	if err != nil {
+		http.Error(w, "Invalid request payload", http.StatusBadRequest)
+		return
+	}
+
+	err = models.UpdateAddress(addressID, &address)
+	if err != nil {
+		if errors.Is(err, models.ErrAddressNotFound) {
+			http.Error(w, "Address not found", http.StatusNotFound)
+			return
+		}
+		http.Error(w, "Failed to update address", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func deleteAddressHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	addressID, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		http.Error(w, "Invalid address ID", http.StatusBadRequest)
+		return
+	}
+
+	err = models.DeleteAddress(addressID)
+	if err != nil {
+		if errors.Is(err, models.ErrAddressNotFound) {
+			http.Error(w, "Address not found", http.StatusNotFound)
+			return
+		}
+		http.Error(w, "Failed to delete address", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
 }
